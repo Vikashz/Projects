afb3475778e8567c794bbafbc90287ee016b6aa4
Vikash <vk925168@gmail.com>
2020-06-20 15:32:37 +0530

    Rendered Templates

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask,request,jsonify
+from flask import Flask,render_template,request,jsonify
+import os
 # from werkzeug import secure_filename
 import codecs
 
@@ -10,22 +11,26 @@ def user(file_name):
     if file_name =="file2.txt":
         with codecs.open(file_name, 'rb', encoding='utf-16be',errors='ignore') as file:
             contents = file.read()
-            return jsonify(contents)
+        # return jsonify(contents)
+        return render_template("display.html", content=contents)
     
     elif file_name =="file3.txt":
         with codecs.open(file_name, 'rb', encoding='us-ascii',errors='ignore') as file:
             contents = file.read()
-            return jsonify(contents)
+            # return jsonify(contents)
+        return render_template("display.html", content=contents)
     
     elif file_name =="file4.txt":
         with codecs.open(file_name, 'rb', encoding='utf-16le',errors='ignore') as file:
             contents = file.read()
-            return jsonify(contents)
+            # return jsonify(contents)
+        return render_template("display.html", content=contents)
     
     else:
         with codecs.open("file1.txt", 'rb', encoding='us-ascii',errors='ignore') as file:
             contents = file.read()
-            return jsonify(contents)
+            # return jsonify(contents)
+        return render_template("display.html", content=contents)
 
 if __name__ == '__main__':
     app.run(debug = True)
