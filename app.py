13eea6af732f7e67cd6b85ce94a6973f57e6d88e
Vikash <vk925168@gmail.com>
2020-06-20 17:13:44 +0530

    Added start line and end line

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
 from flask import Flask,render_template,request,jsonify
 import os
-# from werkzeug import secure_filename
 import codecs
 
 app= Flask(__name__)
 
-@app.route('/', defaults={'file_name': "file1.txt"})
-@app.route('/<file_name>')
-def user(file_name):
+@app.route('/', defaults={'file_name': "file1.txt",'start_line_no': "0",'end_line_no':"150"})
+@app.route('/<file_name>',defaults={'start_line_no': "0", 'end_line_no':"150"})
+@app.route('/<file_name>/<start_line_no>',defaults={'end_line_no':"150"})
+@app.route('/<file_name>/<start_line_no>/<end_line_no>')
+def user(file_name,start_line_no,end_line_no):
     if file_name =="file2.txt":
-        with codecs.open(file_name, 'rb', encoding='utf-16be',errors='ignore') as file:
-            contents = file.read()
-        # return jsonify(contents)
-        return render_template("display.html", content=contents)
+        if int(start_line_no) < int(end_line_no):
+            s=int(start_line_no)
+            e=int(end_line_no)
+            txt=''
+            with codecs.open(file_name, 'rb', encoding='utf-16be',errors='ignore') as file:
+                for i, line in enumerate(file):
+                    if(i>=s and i<e+1):
+                        txt= txt+line
+            return render_template("display.html", content=txt)
+        else:    
+            with codecs.open(file_name, 'rb', encoding='utf-16be',errors='ignore') as file:
+                contents = file.read()
+            # return jsonify(contents)
+            return render_template("display.html", content=contents)
     
     elif file_name =="file3.txt":
-        with codecs.open(file_name, 'rb', encoding='us-ascii',errors='ignore') as file:
-            contents = file.read()
+        if int(start_line_no) < int(end_line_no):
+            s=int(start_line_no)
+            e=int(end_line_no)
+            txt=''
+            with codecs.open(file_name, 'rb', encoding='us-ascii',errors='ignore') as file:
+                for i, line in enumerate(file):
+                    if(i>=s and i<e+1):
+                        txt= txt+line
+            return render_template("display.html", content=txt)
+        else:    
+            with codecs.open(file_name, 'rb', encoding='us-ascii',errors='ignore') as file:
+                contents = file.read()
             # return jsonify(contents)
-        return render_template("display.html", content=contents)
+            return render_template("display.html", content=contents)
     
     elif file_name =="file4.txt":
         with codecs.open(file_name, 'rb', encoding='utf-16le',errors='ignore') as file:
@@ -27,10 +48,20 @@ def user(file_name):
         return render_template("display.html", content=contents)
     
     else:
-        with codecs.open("file1.txt", 'rb', encoding='us-ascii',errors='ignore') as file:
-            contents = file.read()
-            # return jsonify(contents)
-        return render_template("display.html", content=contents)
+            if int(start_line_no) < int(end_line_no):
+                s=int(start_line_no)
+                e=int(end_line_no)
+                txt=''
+                with codecs.open("file1.txt", 'rb', encoding='us-ascii',errors='ignore') as file:
+                    for i, line in enumerate(file):
+                        if(i>=s and i<e+1):
+                            txt= txt+line
+                return render_template("display.html", content=txt)
+            else:    
+                with codecs.open("file1.txt", 'rb', encoding='us-ascii',errors='ignore') as file:
+                    contents = file.read()
+                # return jsonify(contents)
+                return render_template("display.html", content=contents)
 
 if __name__ == '__main__':
     app.run(debug = True)
